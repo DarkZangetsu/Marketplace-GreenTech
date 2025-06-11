a89ea2e34e57170ddbc2de408047a2504a55923b
DarkZangetsu <113225391+DarkZangetsu@users.noreply.github.com>
2025-06-11 21:18:50 +0300

    update settings

--- greentech/greentech/settings.py
+++ greentech/greentech/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'token'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','https://marketplace-greentech.onrender.com/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
