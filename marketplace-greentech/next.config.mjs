334b63b0091394d2defab8cb452a1ce26cabd355
DarkZangetsu <113225391+DarkZangetsu@users.noreply.github.com>
2025-06-12 06:12:17 +0300

    env

--- marketplace-greentech/next.config.mjs
+++ marketplace-greentech/next.config.mjs
@@ -44,7 +44,7 @@ const nextConfig = {
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
+            value: 'origin-when-cross-origin',J
           },
         ],
       },
